be150939c6c774d8ac18de727a44aa0c7599b9dd
filbertlai <47244149+filbertlai@users.noreply.github.com>
2019-04-24 21:14:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@ using namespace std;
 
 int main()
 {
-    string name = "Apple";
-    int num_of_x = (50-name.length())/2;
+    string name = "Test";
+    int number_of_x=50,num_of_x = (50-name.length()-36)/2;;
+    if ((50-name.length()-36)%2==1) {number_of_x--;}
     while (1)
     {
-        cout<<setfill('x')<<setw(50)<<"x"<<endl;
-        cout<<'x'*num_of_x<<"Welcome back! "<<name<<"What can we help you?"<<'x'*num_of_x<<endl;
+        cout<<setfill('x')<<setw(number_of_x)<<"x"<<endl;
+        for(int i=0;i<num_of_x;i++)
+        {
+            cout<<"x";
+        }
+        cout<<"Welcome back! "<<name<<" What can we help you?";
+        for(int i=0;i<num_of_x;i++)
+        {
+            if(i==num_of_x-1){cout<<"x"<<endl; break;}
+            cout<<"x";
+            
+        }
     }
     return 0;
 }
