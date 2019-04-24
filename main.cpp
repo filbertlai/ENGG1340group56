05311907d93ddb41d6f827f5266979e156971c90
vanessa721 <48952254+vanessa721@users.noreply.github.com>
2019-04-24 20:10:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-while (True)
-{
-  cout<<setfill('-')<<endl;
-  return 0;
+int main(){
+    string name = "Apple";
+    int num_of_x = (60-name.length())/2;
+    while (True){
+        cout<<setfill('x')<<setw(60)<<"x"<<endl;
+        cout<<x*num_of_x<<"Welcome back! "<<name<<"What can we help you?"<<x*num_of_x<<endl;
+    return 0;
+    }
 }
