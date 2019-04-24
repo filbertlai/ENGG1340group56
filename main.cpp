0f35864f025f9acbcbdeeeb82f0d3fbf1255eafd
filbertlai <47244149+filbertlai@users.noreply.github.com>
2019-04-24 20:19:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <iomanip>
 using namespace std;
 
-int main(){
+int main()
+{
     string name = "Apple";
-    int num_of_x = (60-name.length())/2;
-    while (True){
-        cout<<setfill('x')<<setw(60)<<"x"<<endl;
-        cout<<x*num_of_x<<"Welcome back! "<<name<<"What can we help you?"<<x*num_of_x<<endl;
-    return 0;
+    int num_of_x = (50-name.length())/2;
+    while (1)
+    {
+        cout<<setfill('x')<<setw(50)<<"x"<<endl;
+        cout<<'x'*num_of_x<<"Welcome back! "<<name<<"What can we help you?"<<'x'*num_of_x<<endl;
     }
+    return 0;
 }
