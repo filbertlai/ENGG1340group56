a777ca2db1b265aa8a20d67ee41c5edbb300b249
filbertlai <47244149+filbertlai@users.noreply.github.com>
2019-04-25 18:21:53 +0800

    Update report.cpp

--- report.cpp
+++ report.cpp
@@ -1,2 +1,47 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
+
+int main()
+{
+    //input text files to add income and expense to income_sum and expense_sum respectively
+    
+    
+    double income_sum,expense_sum;
+    cout<<"XXXXX Welcome to the statistic report of your financial state XXXXX"<<endl;
+    cout<<"Your monthly income in the current month: "<<income_sum<<endl;
+    cout<<"Your monthly expenses in the current month: "<<expense_sum<<endl;
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly income in :"<<;
+    cout<<"The percentage of in your total income is :"<<<<"%";
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+    cout<<"Your monthly expense in :"<<;
+}
