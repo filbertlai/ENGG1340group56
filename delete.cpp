9ac2d4d1bd81ee2f674b15f2dc8909f8eb7dde25
vanessa721 <48952254+vanessa721@users.noreply.github.com>
2019-04-25 21:18:02 +0800

    Update delete.cpp

--- delete.cpp
+++ delete.cpp
@@ -13,17 +13,55 @@ void delet() {
 	}
 	if (choice == 'I') {
 		ifstream fin;
-		fin.open("income.txt", ios::app);
+		fin.open("income.txt");
 		if (fin.fail()) {
 			cout << "Error!" << endl;
 		}
 		string line;
 		int i = 1;
-		while (getline(fin,line)) {
+		while (getline(fin, line)) {
 			cout << i << ". " << line << endl;
 			i += 1;
 		}
+		cout << "Please type in the line number that you would like to delete: ";
+		int line_number;
+		cin >> line_number;
+		fin.close();
+
+		//open new file
+		ofstream temp;
+		ifstream old;
+		old.open("income.txt");
+		temp.open("temp.txt");
+		if (temp.fail()) {
+			cout << "Error!" << endl;
+		}
+		int max = i + 1, a = 0;
+		string writing;
+		while (getline(old, writing)) {
+			a += 1;
+			if (a != line_number) {
+				temp << writing << endl;
+			}
+		}
 	}
+		if (choice == 'E') {
+			ifstream ex;
+			ex.open("expense.txt");
+			if (ex.fail()) {
+				cout << "Error!" << endl;
+			}
+			string line;
+			int i = 1;
+			while (getline(ex, line)) {
+				cout << i << ". " << line << endl;
+				i += 1;
+			}
+			cout << "Please type in the line number that you would like to delete: ";
+				int line_number;
+			cin >> line_number;
+			ex.close();
+		}
 }
 int main() {
 	delet();
