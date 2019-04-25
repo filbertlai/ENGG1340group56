ca36ee8792408293f28cae7e381ad74596c8b8f8
vanessa721 <48952254+vanessa721@users.noreply.github.com>
2019-04-25 17:47:46 +0800

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -12,8 +12,8 @@ void add() {
 		cin >> choice;
 	}
 	if (choice == 'I') {
-		string Year, Month, Day, catergories, description;
-		int num_of_categories, amount;
+		string catergories, description;
+		int Year, Month, Day, num_of_categories, amount;
 		ofstream fout;
 		fout.open("income.txt", ios::app);
 		if (fout.fail()) {
@@ -26,7 +26,7 @@ void add() {
 		cout << "Day:";
 		cin >> Day;
 		while (Day > 31) {
-			cout << "Invalid day! Please input again." << endl;
+			cout << "Invalid day! Please input again. Day: ";
 			cin >> Day;
 		}
 		cout << "Please choose the catergory from the below choices:" << endl;
@@ -36,11 +36,11 @@ void add() {
 		cout << "4.Pocket Money" << endl;
 		cout << "5.Gifts" << endl;
 		cout << "6.Lottery prizes" << endl;
-		cout << "Please indicate your choice in 1-6:" << endl;
+		cout << "Please indicate your choice in 1-6: ";
 		cin >> num_of_categories;
 		// Check whether the input number is in the range of 1-6
 		while ((num_of_categories > 6) || (num_of_categories < 1)) {
-			cout << "Incorrect number!Please input a number from 1 to 6" << endl;
+			cout << "Incorrect number!Please input a number from 1 to 6: ";
 			cin >> num_of_categories;
 		}
 		//input the categories rather than just the number
@@ -64,11 +64,36 @@ void add() {
 			catergories == "6.Lottery prizes";
 			break;
 		}
-		cout << "Description:" << endl;
+		cout << "Description:";
 		cin >> description;
-		cout << "Amount(in HKD $):" << endl;
+		cout << "Amount(in HKD $):";
 		cin >> amount;
+		cout << endl;
 		fout << Year << " " << Month << " " << Day << " " << catergories << " " << description << " " << amount << endl;
+	}
+	if (choice == 'E') {
+		string catergories, description;
+		int Year, Month, Day, num_of_categories, amount;
+		ofstream expense;
+		expense.open("expense.txt", ios::app);
+		if (expense.fail()) {
+			cout << "Error!" << endl;
+		}
+		cout << "Year:";
+		cin >> Year;
+		cout << "Month:";
+		cin >> Month;
+		cout << "Day:";
+		cin >> Day;
+		while (Day > 31) {
+			cout << "Invalid day! Please input again." << endl;
+			cin >> Day;
+		}
+		cout << "Please choose the catergory from the below choices:" << endl;
+		cout << "1. Food & Beverage" << endl;
+		cout << "2. Transport" << endl;
+		cout << "3. Personal care" << endl;
+		cout << "4. Food & Beverage" << endl;
 	}
 }
 
