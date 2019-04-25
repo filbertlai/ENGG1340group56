82098f90b37bf5ad73fc475f72494ea5c061a361
vanessa721 <48952254+vanessa721@users.noreply.github.com>
2019-04-25 20:03:10 +0800

    Update delete.cpp

--- delete.cpp
+++ delete.cpp
@@ -1,2 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
+
+void delet() {
+	cout << setfill('x') << setw(50) << "x" << endl;
+	char choice = 'A';
+	while (choice != 'I' && choice != 'E') {
+		cout << "Income or Expense? Please type in I or E" << endl;
+		cin >> choice;
+	}
+	if (choice == 'I') {
+		ifstream fin;
+		fin.open("income.txt", ios::app);
+		if (fin.fail()) {
+			cout << "Error!" << endl;
+		}
+    fin >> year >> month >> day >> cetegory >> description >> amount
+    cout << year << "/" << month << "/" << day << " " << cetegory << " " <<description << " " << "$" << amount <<endl;
