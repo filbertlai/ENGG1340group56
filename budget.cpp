f420b28776d4fef07752ab3e8919bd6dc2fbac1d
filbertlai <47244149+filbertlai@users.noreply.github.com>
2019-04-27 19:51:13 +0800

    Update budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -2,6 +2,13 @@
 #include<fstream>
 using namespace std;
 
+//This is a function to set monthly budget
+//User is required to input year, month and budget
+//A text file named "budget.txt" will be generated to record the budget plan then
+//For example, if a user want to set a budget of $1000 in April, 2019.
+//Then, the user is required to input (2019 -> 4 -> 1000)
+//After the input, "budget.txt" will be generated with content (2019 4 $1000)
+
 int main()
 {
     int year, month;
@@ -17,10 +24,11 @@ int main()
     fout.open("budget.txt");
     if (fout.fail())
     {
-        cout<<"Error!"<<endl;
+        cout<<"Error in setting the file!"<<endl;
         exit(1);
     }
     fout<<year<<" "<<month<<" $"<<budget;
     fout.close();
     cout<<"Budget setted successfully!"<<endl;
+    cout<<"The budget you setted is: $"<<budget<<" in "<<month<<" of "<<year<<".";
 }
