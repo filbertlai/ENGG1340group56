c064a33ad4d8d7421e929c621e984353f6b64d52
filbertlai <47244149+filbertlai@users.noreply.github.com>
2019-04-27 19:08:13 +0800

    Update budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -1,2 +1,26 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
+
+int main()
+{
+    int year, month;
+    double budget;
+    cout<<"Welcome to set your monthly budget!"<<endl;
+    cout<<"The year of the budget plan: ";
+    cin>>year;
+    cout<<"The month of the budget plan: ";
+    cin>>month;
+    cout<<"The amount of budget of the month you inputted: ";
+    cin>>budget;
+    ofstream fout;
+    fout.open("budget.txt");
+    if (fout.fail())
+    {
+        cout<<"Error!"<<endl;
+        exit(1);
+    }
+    fout<<year<<" "<<month<<" $"<<budget;
+    fout.close();
+    cout<<"Budget setted successfully!"<<endl;
+}
