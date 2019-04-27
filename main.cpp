27cbfbd2372117e8620efa6a2d07223d4b04a80e
vanessa721 <48952254+vanessa721@users.noreply.github.com>
2019-04-27 22:22:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,29 +29,18 @@ int main()
         cout<<"3)  Edit a record"<<endl;
         cout<<endl<<"****Looking for specific record(s)/statistic report****"<<endl;
         cout<<"4)  View records of specific category or account"<<endl;
-        cout<<"5)  View records sorted by date or amount"<<endl;
-        cout<<"6)  Search for specific record(s)"<<endl;
-        cout<<"7)  Show Statistic report of financial state"<<endl;
+        cout<<"5)  Search for specific record(s)"<<endl;
+        cout<<"6)  Show Statistic report of financial state"<<endl;
         cout<<endl<<"*****************Preset budget/records*****************"<<endl;
-        cout<<"8)  Set a budget for the next month"<<endl;
-        cout<<"11) Preset the regular income or expenses"<<endl;
+        cout<<"7)  Set a budget for the next month"<<endl;
         cout<<endl<<"Please enter the number of command: ";
         cin>>cmd;
-		if (cmd == 1) { add(); }
-        
-        else if (cmd==2) {delet(); }
-        
-        else if (cmd==3){ edit(); }
-        
-		else if (cmd == 4) { category(); }
-        
-		else if (cmd == 5) { }
-        
-		else if (cmd == 6){searchh(); }
-        
-		else if (cmd == 7) { report(); }
-        
-		else if (cmd == 8) { budget(); }
-    }
+	    if (cmd == 1) { add(); }
+	    else if (cmd==2) {delet(); }
+	    else if (cmd==3){ edit(); }
+	    else if (cmd == 4) { category(); }
+	    else if (cmd == 5){searchh(); }
+	    else if (cmd == 6) { report(); }
+	    else if (cmd == 7) { budget(); }
     return 0;
 }
